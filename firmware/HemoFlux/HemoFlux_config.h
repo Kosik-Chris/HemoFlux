02b88b42e8a220f8be805d51e1c6e77910412cff
kyt <kosik.chris.j@gmail.com>
2020-03-23 14:36:06 -0400

    Firmware updates: Added IMU calibration, BLE broadcasting. Updated channel BLE services to single channel service vs. service for each wavelength TODO: fix endianess of the BLE transmission (backwards)

--- firmware/HemoFlux/HemoFlux_config.h
+++ firmware/HemoFlux/HemoFlux_config.h
@@ -1,4 +1,5 @@
 #define DEBUG
+#define OUTPUT_READABLE_ACCELGYRO
 #define MAX_NUM_PPG 7 //8 -1 (i2c gyro/accel)
 #define NUM_PPG 1
 #define MAX_CHAR_HANDLE 25 //max number chars per service
@@ -10,6 +11,7 @@
 
 #define TCAADDR 0x70
 #define MPUADDR 0x69
+
 //Setup to sense up to 18 inches, max LED brightness
 #define ledBrightness 0xFF //Options: 0=Off to 255=50mA
 #define sampleAverage 1 //Options: 1, 2, 4, 8, 16, 32
@@ -19,7 +21,7 @@
 #define adcRange 16384 //Options: 2048, 4096, 8192, 16384
 
 //BLE DEFINITIONS
-#define RED_SERV_UUID BLEUUID("0265204d-6cfd-4be7-8548-25f0f941b794")
+#define CHANNEL_SERV_UUID BLEUUID("0265204d-6cfd-4be7-8548-25f0f941b794")
 #define REDCHAR_0_CHAR_UUID "050447d6-8ac9-4bd8-b004-0a5fe425029a"
 #define REDCHAR_1_CHAR_UUID "59eb1a6f-9839-483d-90aa-511b96585820"
 #define REDCHAR_2_CHAR_UUID "031edcf6-02c9-4267-a657-91eacd0febc8"
@@ -28,7 +30,7 @@
 #define REDCHAR_5_CHAR_UUID "67051d05-68ac-4748-840b-b835600f2ad7"
 #define REDCHAR_6_CHAR_UUID "f9c35962-b1d1-40a8-8766-0d9e6f1fb1ea"
 
-#define IR_SERV_UUID BLEUUID("a9e81533-d3b4-4b20-9c34-6d817942b69a")
+//#define IR_SERV_UUID BLEUUID("a9e81533-d3b4-4b20-9c34-6d817942b69a")
 #define IRCHAR_0_CHAR_UUID "654859cc-9a09-453f-b87a-8493f0fc1890"
 #define IRCHAR_1_CHAR_UUID "ec402230-01bb-4013-85a2-82df61e7b869"
 #define IRCHAR_2_CHAR_UUID "392e2b46-ac60-4807-a329-016c65f09a28"
@@ -37,7 +39,7 @@
 #define IRCHAR_5_CHAR_UUID "8844f336-3974-45c3-b582-c3369baf3f27"
 #define IRCHAR_6_CHAR_UUID "fdbddd27-6328-4cc1-bb0d-666a1b4fb8eb"
 
-#define GREEN_SERV_UUID BLEUUID("08fcbfeb-ea38-4085-9800-cd03ff14f2a0")
+//#define GREEN_SERV_UUID BLEUUID("08fcbfeb-ea38-4085-9800-cd03ff14f2a0")
 #define GREENCHAR_0_CHAR_UUID "686af018-8d13-4dd0-afe1-fecbdd054219"
 #define GREENCHAR_1_CHAR_UUID "711c47c2-3b96-4fee-ace5-fad82ea2f41b"
 #define GREENCHAR_2_CHAR_UUID "e344ff91-2d0c-43fe-bf4c-41bf78047cdf"
@@ -58,6 +60,28 @@
 #define HARDWAREV_CHAR_UUID BLEUUID((uint16_t)0x2A27)
 #define FIRMWAREV_CHAR_UUID BLEUUID((uint16_t)0x2A26)
 #define SYSTEMID_CHAR_UUID BLEUUID((uint16_t)0x2A23)
+
+#define DIMENSIONS_SERV_UUID BLEUUID("40940496-19d7-4c8f-9057-f87bb56c6210") //service to hold orientation and accel vectors
+#define AX_CHAR_UUID "9f0f16e4-3a52-4669-9f6f-6415083429de"
+#define AY_CHAR_UUID "b3a77cdb-5015-4ccd-abbf-0fde8541b75d"
+#define AZ_CHAR_UUID "07c4d47a-e25b-416a-a39f-14727cb9d062"
+#define GX_CHAR_UUID "d1b604bf-351c-444c-98d0-49a200bcb7e4"
+#define GY_CHAR_UUID "bd19e5d0-2a47-4407-b7ab-6d6f20b15bd7"
+#define GZ_CHAR_UUID "852b8b8d-3a64-4733-996b-b0f55452f592"
+
+//IMU calibration
+#define imu_buffersize 20     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
+#define acel_deadzone 20          //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
+#define giro_deadzone 1
+
+//typedef struct _dimensions {
+//  uint16_t ax;
+//  uint16_t ay;
+//  uint16_t az;
+//  uint16_t gx;
+//  uint16_t gy;
+//  uint16_t gz;
+//}dimensions;
 
 //type to handle all i2c devices attached to esp32
 //typedef struct _i2cslave {
